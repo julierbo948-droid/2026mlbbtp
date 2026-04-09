59feaeaf92491e8173e3720a3b8778638f7bf3e6
julierbo948-droid <julierbo948@gmail.com>
2026-04-09 09:03:59 +0700

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -287,7 +287,7 @@ async def execute_buy_process(message, lines, regex_pattern, currency, packages_
                     report += f"━━━━━━━━━━━━━━━━━━━━━\n"
                     report += f"GAME ID      : {res['game_id']} {res['zone_id']}\n"
                     report += f"IG NAME      : {safe_ig_name}\n"
-                    report += f"ITEM         : {pr['pkg_name']} 💎| ✅\n"
+                    report += f"ITEM         : {pr['pkg_name']} 💎 ✅\n"
                     report += f"SERIAL       :\n{pr['order_ids']}\n"
                     report += f"SPENT        : {pr['spent']:.2f} 🪙\n\n"
                     
@@ -321,7 +321,7 @@ async def execute_buy_process(message, lines, regex_pattern, currency, packages_
                     report += f"━━━━━━━━━━━━━━━━━━━━━\n"
                     report += f"GAME ID      : {res['game_id']} {res['zone_id']}\n"
                     report += f"IG NAME      : {safe_ig_name}\n"
-                    report += f"ITEM         : {pr['pkg_name']} 💎| ❌\n"
+                    report += f"ITEM         : {pr['pkg_name']} 💎 ❌\n"
                     report += f"ERROR        : {display_err}\n\n"
 
             report += f"━━━━━━━━━━━━━━━━━━━━━\n"
